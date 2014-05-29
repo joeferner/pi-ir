06441e1e903ca662c44ba96a52079461fdf1aa07
Joe Ferner <joe@fernsroth.com>
2014-05-29 16:43:25 -0400

    better layour

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-
 var lircjs = require('lircjs');
 
 var lirc = lircjs.connect();
 
 lirc.on('connect', function() {
   console.log('lirc connected');
+
+  // get a list of remotes
+  //lirc.remotes(console.log);
+
+  // get a list of remote commands
+  //lirc.remoteCommands('mceusb', console.log);
+
+  // send IR command
+  lirc.sendOnce('mceusb', 'KEY_1', console.error);
+
+  // send multiple IR command
+  lirc.sendOnce('mceusb', ['KEY_1', 'KEY_2'], console.error);
+
+  // simulate IR command
+  lirc.simulate('0000000000007bfe', 0, 'mceusb', 'KEY_1', console.error);
+
+  setTimeout(function() {
+    lirc.close();
+  }, 1000);
 });
 
 lirc.on('error', function(err) {
@@ -18,14 +36,3 @@ lirc.on('end', function(err) {
 lirc.on('code', function(code) {
   console.log('lirc code', code);
 });
-
-//lirc.remotes(console.log);
-//lirc.remoteCommands('mceusb', console.log);
-
-//lirc.sendOnce('remote1', 'code1', console.error);
-//lirc.sendOnce('remote1', ['code1', 'code2'], console.error);
-lirc.simulate('0000000000007bfe', 0, 'KEY_1', 'mceusb', console.error);
-
-setTimeout(function() {
-  lirc.close();
-}, 1000);
