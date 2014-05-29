8f067258f709563ec5ba127965443031c3821993
Joe Ferner <joe@fernsroth.com>
2014-05-29 16:38:54 -0400

    added more commands end events

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ lirc.on('error', function(err) {
   console.log('lirc error', err);
 });
 
+lirc.on('end', function(err) {
+  console.log('lirc end');
+});
+
 lirc.on('code', function(code) {
   console.log('lirc code', code);
 });
 
-lirc.remotes(console.log);
-lirc.remoteCommands('mceusb', console.log);
+//lirc.remotes(console.log);
+//lirc.remoteCommands('mceusb', console.log);
 
-lirc.sendOnce('remote1', 'code1', console.error);
-lirc.sendOnce('remote1', ['code1', 'code2'], console.error);
+//lirc.sendOnce('remote1', 'code1', console.error);
+//lirc.sendOnce('remote1', ['code1', 'code2'], console.error);
+lirc.simulate('0000000000007bfe', 0, 'KEY_1', 'mceusb', console.error);
+
+setTimeout(function() {
+  lirc.close();
+}, 1000);
